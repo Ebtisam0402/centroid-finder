0283fa9e47d90f591a35878a536dcb1efffb7a22
Azeb Tesfay <azbseb@gmail.com>
2026-05-01 20:28:57 -0700

    AI USED code for testDfsBinaryGroupFinderMultipleGroupsSortedDescending

--- src/DfsBinaryGroupFinderTest.java
+++ src/DfsBinaryGroupFinderTest.java
@@ -44,18 +44,43 @@ public class DfsBinaryGroupFinderTest {
     @Test
     public void testTwoSeparateGroupsSortedDescending() {
         int[][] image = {
-            {1, 1, 0, 0},
-            {1, 0, 0, 1},
-            {0, 0, 0, 1}
+                { 1, 1, 0, 0 },
+                { 1, 0, 0, 1 },
+                { 0, 0, 0, 1 }
         };
 
         DfsBinaryGroupFinder finder = new DfsBinaryGroupFinder();
 
         List<Group> expected = List.of(
-            new Group(3, new Coordinate(0, 0)),
-            new Group(2, new Coordinate(3, 1))
-        );
+                new Group(3, new Coordinate(0, 0)),
+                new Group(2, new Coordinate(3, 1)));
 
         assertEquals(expected, finder.findConnectedGroups(image));
+    }
+
+    @Test
+    public void testDfsBinaryGroupFinderMultipleGroupsSortedDescending() {
+        int[][] image = {
+                { 1, 1, 0, 1 },
+                { 0, 1, 0, 1 },
+                { 0, 0, 0, 1 }
+        };
+
+        BinaryGroupFinder finder = new DfsBinaryGroupFinder();
+
+        List<Group> groups = finder.findConnectedGroups(image);
+
+        assertEquals(2, groups.size());
+
+        Group first = groups.get(0);
+        Group second = groups.get(1);
+
+        assertEquals(3, first.size());
+        assertEquals(3, first.centroid().x());
+        assertEquals(1, first.centroid().y());
+
+        assertEquals(3, second.size());
+        assertEquals(0, second.centroid().x());
+        assertEquals(0, second.centroid().y());
     }
 }
