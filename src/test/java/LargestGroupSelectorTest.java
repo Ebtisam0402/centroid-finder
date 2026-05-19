068ae44ffc157f3389ab5ec79b4e2c8def5daf92
Azeb Tesfay <azbseb@gmail.com>
2026-05-19 14:56:24 -0700

    selectLargestReturnsOnlyGroupWhenListHasOneGroup

--- src/test/java/LargestGroupSelectorTest.java
+++ src/test/java/LargestGroupSelectorTest.java
@@ -1,7 +1,9 @@
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.lang.foreign.Linker.Option;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
@@ -23,6 +25,18 @@ public class LargestGroupSelectorTest {
   Optional<Group> result = selector.selectLargest(new ArrayList<>());
 
   assertTrue(result.isEmpty());
+ }
+
+ @Test
+ public void selectLargestReturnsOnlyGroupWhenListHasOneGroup() {
+  LargestGroupSelector selector = new LargestGroupSelector();
+
+  Group group = new Group(1, new Coordinate(1, 1));
+
+  Optional<Group> result = selector.selectLargest(List.of(group));
+
+  assertTrue(result.isPresent());
+  assertSame(group, result.get());
  }
 
 }
