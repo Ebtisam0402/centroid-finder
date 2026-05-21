a5b396c7838b9c28a8a94ce210a4166553408b3d
Ebtisam Jelani <93232997+Ebtisam0402@users.noreply.github.com>
2026-05-20 21:18:24 -0700

    added csvWriter

--- src/main/java/CsvWriter.java
+++ src/main/java/CsvWriter.java
@@ -1,3 +1,59 @@
-public class CsvWriter {
 
+/*
+ * This class writes centroid information into a CSV file.
+ * Each row in the CSV will look like:
+ *
+ * seconds,x,y
+ * 0.0,100,200
+ * 1.0,-1,-1
+ */
+
+import java.io.File;
+import java.io.PrintWriter;
+
+public class CsvWriter {
+    //PrinterWriter is used to wwrite text into a file
+    private PrintWriter writer;
+
+     /*
+     * Constructor
+     * Runs when a new CentroidCsvWriter object is created.
+     *
+     * outputPath is the name/path of the CSV file.
+     */
+
+     public CsvWriter(String outputPath) throws Exception {
+
+        // Create a File object for the output CSV file.
+        File outputFile = new File(outputPath);
+
+        // Connect PrintWriter to the file so we can write into it.
+        writer = new PrintWriter(outputFile);
+
+        //write the csv header row.
+        writer.println("seconds,x,y");
+
+
+     }
+
+       /*
+     * Writes one row into the CSV file.
+     *
+     * Example:
+     * 0.5,120,300
+     */
+
+    public void writeRow(double seconds, int x, int y) {
+        //Combine the values into one csv line separated by commas.
+        writer.println(seconds + "," + x + ","+y);
+    }
+
+    /*
+     * Closes the file after writing is finished.
+     * This is important so all data is properly saved.
+     */
+    public void close() {
+        //close the PrintWriter connection to the file.
+        writer.close();
+    }
 }
