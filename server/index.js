60df2d74b31f983dc8262bd6548de632ddc37fae
Azeb Tesfay <azbseb@gmail.com>
2026-05-23 08:09:54 -0700

    Reorganize project into server and processor folders

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+require("dotenv").config({ path: "../.env" })
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.get("/", (req, res) => {
+ res.json({
+  message: "Salamander server running"
+ })
+})
+
+app.listen(PORT, () => {
+ console.log(`Server listening on port ${PORT}`)
+})
